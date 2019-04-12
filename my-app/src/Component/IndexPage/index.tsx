4517b26eedff81386fa82df96cb3834a162e7d3d
sakamoto.m <sakamoto.m@water-cell.jp>
2019-04-12 18:39:58 +0900

    進めた

--- my-app/src/Component/IndexPage/index.tsx
+++ my-app/src/Component/IndexPage/index.tsx
@@ -1,14 +1,46 @@
 import React from "react";
-import axios from "axios";
 import {backlogApi} from "../../lib/backlog-settings";
+import Select from "react-select";
 
+type IndexStateTypes = {
+  projects: object[];
+  issues: object[];
+}
 
-export class IndexPage extends React.Component {
+export class IndexPage extends React.Component<any, IndexStateTypes, any> {
+  constructor(props: any) {
+    super(props)
+    this.state = {
+      projects: [],
+      issues: [],
+    };
+  }
+
   render(): React.ReactElement<any, string | React.JSXElementConstructor<any>> | string | number | {} | React.ReactNodeArray | React.ReactPortal | boolean | null | undefined {
-    return <div>あああ</div>;
+    const optionsForProjects = this.state.projects.map((p: any) => ({
+      value: p.id,
+      label: p.name
+    }));
+    return (
+      <div>
+        <Select options={optionsForProjects} onChange={this.handleChangeProject} />
+        {this.state.issues.map((p: any) => {
+          return <pre>{p.summary}</pre>;
+        })}
+      </div>
+    );
   }
   componentDidMount(): void {
-    backlogApi.getProjects().then(r => console.log(r));
-    backlogApi.getIssues()
+    backlogApi.getProjects().then(r => this.setState({projects: r}));
+  }
+
+  handleChangeProject = (event: any) => {
+    backlogApi.getIssues({
+      projectId: [event.value],
+      statusId: [1, 2],
+      keyword: ""
+    }).then((issues) => this.setState({
+      issues
+    }))
   }
 }
