a85b3cdcb119f19ee722d45edd5b6b18112d7f7f
sakapun <sakapun5th@gmail.com>
2019-04-21 17:25:36 +0900

    ヘッダー調整

--- src/presentation/component/Header/index.tsx
+++ src/presentation/component/Header/index.tsx
@@ -1,28 +1,37 @@
+import { Box } from "@smooth-ui/core-em";
 import React, { useCallback } from "react";
-import { useDispatch } from "../../../application/provider";
+import { useDispatch, useGlobalState } from "../../../application/provider";
 import { BACKLOG_API_LOCALSTORAGE_KEY_NAME } from "../../../domain/BacklogSetting";
 import { Button } from "../Button";
 import { AppNameBox, Outer } from "./element";
 
 export type HeaderComponentType = {
   handleClickLogout: () => void;
+  isSettingPage: boolean;
 };
 
-export const HeaderComponent = ({ handleClickLogout }: HeaderComponentType) => {
+export const HeaderComponent = ({ handleClickLogout, isSettingPage }: HeaderComponentType) => {
   return (
     <Outer>
       <AppNameBox>優先度ビューアー</AppNameBox>
-      <Button onClick={handleClickLogout}>logout</Button>
+      <Box flex={1} />
+      {isSettingPage ? null : (
+        <Button onClick={handleClickLogout} variant={"light"} height={1} borderRadius={"unset"}>
+          ログアウト
+        </Button>
+      )}
     </Outer>
   );
 };
 
 export const Header = () => {
+  const isSettingPage = useGlobalState("isSettingPage");
+
   const dispatch = useDispatch();
   const handleClickLogout = useCallback(() => {
     window.localStorage.removeItem(BACKLOG_API_LOCALSTORAGE_KEY_NAME);
     dispatch({ type: "CLEAR_BACKLOG" });
   }, []);
 
-  return <HeaderComponent handleClickLogout={handleClickLogout} />;
+  return <HeaderComponent handleClickLogout={handleClickLogout} isSettingPage={isSettingPage} />;
 };
